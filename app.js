be6db56feb139ba28201e30e9ce10e260139e17d
PintilieGeani <pintilie.geany@yahoo.it>
2025-06-28 15:35:25 +0200

    cors

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ import express from "express";
 import router from "./router/movies.js";
 import errorHandler from "./middleware/errorsHandler.js";
 import notFound from "./middleware/notfound.js";
-
+import cors from "cors"
 const app = express()
 const port = 3000
 
+
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 app.use(express.json());
 app.use(express.static("public"))
 
